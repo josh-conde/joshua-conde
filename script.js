5b68c48718fcef88319721b501370d158d72ad5e
joshconde <joshconde@aol.com>
2024-06-13 13:05:52 -0400

    Class Commit 1

--- script.js
+++ script.js
@@ -1,88 +1,57 @@
 document.addEventListener("DOMContentLoaded", function() {
+    function type(element, text) {
+        let index = 0;
+        let typingInterval = setInterval(function() {
+            if (index < text.length) {
+                element.textContent += text.charAt(index);
+                index++;
+            } else {
+                clearInterval(typingInterval);
+            }
+        }, 100);
+        return typingInterval;
+    }
+
+    function startTyping(element, text) {
+        element.textContent = '';
+        element.style.width = "auto";
+        return type(element, text);
+    }
+
+    function resetTyping(element, text) {
+        clearInterval(element.typingInterval);
+        element.textContent = text;
+    }
+
     const typingText1 = document.querySelector(".typing-text1");
     const typingText2 = document.querySelector(".typing-text2");
     const typingText3 = document.querySelector(".typing-text3");
+
     const text1 = typingText1.getAttribute("data-text");
     const text2 = typingText2.getAttribute("data-text");
     const text3 = typingText3.getAttribute("data-text");
-    let index = 0;
-    let typingInterval;
 
-    function type1() {
-        if (index < text1.length) {
-            typingText1.textContent += text1.charAt(index);
-            index++;
-        } else {
-            clearInterval(typingInterval);
-        }
-    }
+    typingText1.addEventListener("mouseover", function() {
+        typingText1.typingInterval = startTyping(typingText1, text1);
+    });
+    typingText1.addEventListener("mouseout", function() {
+        resetTyping(typingText1, text1);
+    });
 
-    function startTyping1() {
-        typingText1.textContent = '';
-        index = 0;
-        typingText1.style.width = "auto";
-        typingInterval = setInterval(type1, 100);
-    }
+    typingText2.addEventListener("mouseover", function() {
+        typingText2.typingInterval = startTyping(typingText2, text2);
+    });
+    typingText2.addEventListener("mouseout", function() {
+        resetTyping(typingText2, text2);
+    });
 
-    function resetTyping() {
-        clearInterval(typingInterval);
-        typingText1.textContent = '';
-        typingText1.style.width = "0";
-    }
-
-    typingText1.addEventListener("mouseover", startTyping1);
-    typingText1.addEventListener("mouseout", resetTyping);
-
-    function type2() {
-        if (index < text2.length) {
-            typingText2.textContent += text2.charAt(index);
-            index++;
-        } else {
-            clearInterval(typingInterval);
-        }
-    }
-
-    function startTyping2() {
-        typingText2.textContent = '';
-        index = 0;
-        typingText2.style.width = "auto";
-        typingInterval = setInterval(type2, 100);
-    }
-
-    function resetTyping() {
-        clearInterval(typingInterval);
-        typingText2.textContent = '';
-        typingText2.style.width = "0";
-    }
-
-    typingText2.addEventListener("mouseover", startTyping2);
-    typingText2.addEventListener("mouseout", resetTyping);
-
-    function type3() {
-        if (index < text3.length) {
-            typingText3.textContent += text3.charAt(index);
-            index++;
-        } else {
-            clearInterval(typingInterval);
-        }
-    }
-
-    function startTyping3() {
-        typingText3.textContent = '';
-        index = 0;
-        typingText3.style.width = "auto";
-        typingInterval = setInterval(type3, 100);
-    }
-
-    function resetTyping() {
-        clearInterval(typingInterval);
-        typingText3.textContent = '';
-        typingText3.style.width = "0";
-    }
-
-    typingText3.addEventListener("mouseover", startTyping3);
-    typingText3.addEventListener("mouseout", resetTyping);
-    
+    typingText3.addEventListener("mouseover", function() {
+        typingText3.typingInterval = startTyping(typingText3, text3);
+    });
+    typingText3.addEventListener("mouseout", function() {
+        resetTyping(typingText3, text3);
+    });
+});
     document.getElementById('contactForm').addEventListener('submit', function(event) {
         let isValid = true;
         let errorMessage = '';
@@ -121,5 +90,4 @@ document.addEventListener("DOMContentLoaded", function() {
         }else{
             document.getElementById('success').innerHTML = successMessage;
         }
-    });
 });
